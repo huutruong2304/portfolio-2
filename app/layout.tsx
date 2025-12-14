ebfbb4347699d5780de146b9bcf9f181924bb819
Truong Nguyen <38267209+huutruong2304@users.noreply.github.com>
2025-12-14 23:55:37 +0700

    feat: make scroll to top button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/custom/theme-provider'
+import ScrollToTopButton from '@/components/custom/scroll-top-button'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -33,6 +34,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <ScrollToTopButton />
         </ThemeProvider>
       </body>
     </html>
